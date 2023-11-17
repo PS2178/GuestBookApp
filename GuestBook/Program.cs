ea3e9334cdd1473a109f3eb5266f71a090d78085
PS2178 <rcooper1337@gmail.com>
2023-11-17 16:49:52 -0600

    Add project files.

---
+++ GuestBook/Program.cs
@@ -0,0 +1,11 @@
+﻿using GuestBook;
+
+//Console application?
+//Ask the user for their name and how many people are in their party?
+//Track how many people are at the party?
+//Print guest list and number of guests?
+
+StaticMethods.GetName();
+StaticMethods.GetGuests();
+StaticMethods.UpdateGuestBook();
+StaticMethods.PrintGuestBook();
---
+++ GuestBook/StaticMethods.cs
@@ -0,0 +1,45 @@
+﻿using static System.Console;
+
+namespace GuestBook
+{
+    public static class StaticMethods
+    {
+        public static List<string> guests = new();
+        public static string hostName, numberOfGuests, guestName = "";
+
+        public static string GetName()
+        {
+            Write("And what is the name for the party? ");
+            hostName = ReadLine();
+
+            return hostName;
+        }
+
+        public static int GetGuests()
+        {
+            Write("And for how many? ");
+            numberOfGuests = ReadLine();
+
+            return Convert.ToInt32(numberOfGuests);
+        }
+
+        public static void UpdateGuestBook()
+        {
+            for (int i = 0; i < Convert.ToInt32(numberOfGuests); i++)
+            {
+                Write("And what is the guest's name? ");
+                guestName = ReadLine();
+
+                guests.Add(guestName);
+            }
+        }
+
+        public static void PrintGuestBook()
+        {
+            foreach (string guests in guests)
+            {
+                WriteLine(guests);
+            }
+        }
+    }
+}
